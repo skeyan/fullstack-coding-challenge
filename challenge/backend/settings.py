218e25ae4e2c09e37d0b570cdf81c499c1ec4510
Eva Yan <eyan0749@gmail.com>
2024-12-03 16:54:54 -0500

    Enable BrowsableAPIRenderer for testing login auth endpoint

--- challenge/backend/settings.py
+++ challenge/backend/settings.py
@@ -127,5 +127,10 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated', )
+        'rest_framework.permissions.IsAuthenticated', 
+    ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',      
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    )
 }
